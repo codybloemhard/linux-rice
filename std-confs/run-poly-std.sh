7e6567b0897ba8109e15ae946e35d7b72958bad3
Cody Bloemhard <cblmhrd@live.nl>
2019-01-06 00:37:11 +0100

    Polybar bars on all windows, automatic.

--- std-confs/run-poly-std.sh
+++ std-confs/run-poly-std.sh
@@ -6,5 +6,13 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-polybar main &
-
+#polybar main &
+#cool code to aut. load bar on all screens by "apetresc" link below:
+#https://github.com/jaagr/polybar/issues/763
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload main &
+  done
+else
+  polybar --reload example &
+fi
