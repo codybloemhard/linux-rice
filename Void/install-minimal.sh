c6770d722746a0590a4bd77818b4b191abeabf23
Cody Bloemhard <cblmhrd@live.nl>
2019-01-15 20:45:06 +0100

    void polybar install missing fix

--- Void/install-minimal.sh
+++ Void/install-minimal.sh
@@ -3,5 +3,5 @@ echo "Installing (minimal)"
 sudo xbps-install -Suy
 sudo xbps-install -Sy void-repo-multilib void-repo-multilib-nonfree void-repo-nonfree
 sudo xbps-install -S
-sudo xbps-install -Sy xorg i3-gaps i3status dmenu feh compton terminator arc-theme linux-headers
+sudo xbps-install -Sy xorg i3-gaps i3status dmenu feh compton terminator arc-theme linux-headers polybar
 echo "Done"
