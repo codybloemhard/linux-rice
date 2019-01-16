f338450593dbf89a8c97d476ccd204ec5b6420be
Cody Bloemhard <cblmhrd@live.nl>
2019-01-16 19:15:41 +0100

    nvidia drivers and correct screen order

--- Arch/install-minimal.sh
+++ Arch/install-minimal.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Installing (minimal)"
 sudo pacman -Syu
-sudo pacman -S xorg-server xorg-xinit xorg-xrandr i3-gaps i3status dmenu feh compton terminator linux-headers
+sudo pacman -S xorg-server xorg-xinit xorg-xrandr i3-gaps i3status dmenu feh compton terminator linux-headers nvidia
 sudo pacman -S wifi-menu dialog wpa_supplicant
 sudo pacman -S base-devel
 git clone https://aur.archlinux.org/polybar.git
