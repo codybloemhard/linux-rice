79d28c1ee37ab909cdcd5eb811009bf6f15e9688
Cody Bloemhard <cblmhrd@live.nl>
2019-01-29 17:04:02 +0000

    oops

--- void/install-minimal.sh
+++ void/install-minimal.sh
@@ -4,6 +4,9 @@ sudo xbps-install -Suy
 sudo xbps-install -Sy void-repo-multilib void-repo-multilib-nonfree void-repo-nonfree
 sudo xbps-install -S
 sudo xbps-install -Sy xorg i3-gaps dmenu feh compton arc-theme linux-headers polybar vim htop neofetch curl
+# Install ZSH, oh-my-zsh, and needed fonts for latter one
+sudo pacman -S zsh curl
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 git clone https://github.com/powerline/fonts.git --depth=1
 cd fonts
 ./install.sh
