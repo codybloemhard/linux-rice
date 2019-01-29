93fbc7281c85075cb8510dadfa58e3d0154aa30f
Cody Bloemhard <cblmhrd@live.nl>
2019-01-29 18:14:47 +0100

    lolcat

--- void/install-minimal.sh
+++ void/install-minimal.sh
@@ -3,9 +3,9 @@ echo "Installing (minimal)"
 sudo xbps-install -Suy
 sudo xbps-install -Sy void-repo-multilib void-repo-multilib-nonfree void-repo-nonfree
 sudo xbps-install -S
-sudo xbps-install -Sy xorg i3-gaps dmenu feh compton arc-theme linux-headers polybar vim htop neofetch curl
+sudo xbps-install -Sy xorg i3-gaps dmenu feh compton arc-theme linux-headers polybar vim htop neofetch
 # Install ZSH, oh-my-zsh, and needed fonts for latter one
-sudo pacman -S zsh curl
+sudo xbps-install -Sy -S zsh curl lolcat-c
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 git clone https://github.com/powerline/fonts.git --depth=1
 cd fonts
