53aa60fd77271ffc34507a4419f4c02e7835fb24
Cody Bloemhard <cblmhrd@live.nl>
2019-02-17 20:06:15 +0000

    cmus settings

--- arch/config-extra.sh
+++ arch/config-extra.sh
@@ -4,3 +4,4 @@ parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 cd $parent_path
 #cmus
 cp std-confs/cmusrc-std.conf ~/.config/cmus/rc
+cp std-confs/cmus-autosave-std.conf ~/.config/cmus/autosave
