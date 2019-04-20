3cdc2f1d783230a794b42810a3588895e313b6d9
Cody Bloemhard <cblmhrd@live.nl>
2019-04-20 12:34:27 +0100

    from vscode to vscodium

--- arch/install-specific.sh
+++ arch/install-specific.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Installing (specific)"
-sudo pacman -Sy code
 sudo pacman -Sy texlive-most mupdf
 yay -S https://aur.archlinux.org/discord.git
 yay -S https://aur.archlinux.org/pkgbase/minecraft-launcher/
+yay -S vscodium-bin
 echo "Done"
