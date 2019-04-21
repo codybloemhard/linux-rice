1d3b742bd945a4a47889c6de3678cb474fb81662
Cody Bloemhard <cblmhrd@live.nl>
2019-04-21 12:49:57 +0100

    install ag, grep for code

--- arch/install-extra.sh
+++ arch/install-extra.sh
@@ -2,4 +2,5 @@
 echo "Installing (extra)"
 sudo pacman -Sy scrot alsa-firmware alsa-utils firefox sl unzip zathura-pdf-mupdf
 sudo pacman -Sy pulseaudio cmus
+sudo pacman -Sy the_silver_searcher #ag
 echo "Done"
