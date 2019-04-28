7df51e4b46f3782b56cb73cf1fbab5b2d153cd43
Cody Bloemhard <cblmhrd@live.nl>
2019-04-28 23:00:27 +0200

    changed usb and made config-extra make the dirs

--- arch/config-extra.sh
+++ arch/config-extra.sh
@@ -7,3 +7,9 @@ cp std-confs/cmusrc-std.conf ~/.config/cmus/rc
 cp std-confs/cmus-autosave-std.conf ~/.config/cmus/autosave
 #zathura pdf viewer
 cp std-confs/zathurarc.conf ~/.config/zathura/zathurarc
+#directories for usb script
+mkdir ~/mnt
+mkdir ~/mnt/usb0
+mkdir ~/mnt/usb1
+mkdir ~/mnt/usb2
+mkdir ~/mnt/usb3
