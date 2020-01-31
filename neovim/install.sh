20ff99e3d90bda63b54f15bf7f8d2cc4a301152f
Cody Bloemhard <cblmhrd@live.nl>
2020-01-31 21:42:35 +0100

    icons

--- neovim/install.sh
+++ neovim/install.sh
@@ -2,4 +2,5 @@ sudo pacman -Sy --noconfirm nvim
 # plugin manager
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
+# install font to get nerdtree icons to work
+yay -Sy nerd-fonts-complete
