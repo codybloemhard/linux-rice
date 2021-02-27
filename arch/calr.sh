fa22300cf7d911fe66468a3da03c170c74a9fdd8
Cody Bloemhard <cblmhrd@live.nl>
2021-02-28 00:01:51 +0100

    fix rename bug in calr

--- arch/calr.sh
+++ arch/calr.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 cd $parent_path
-sudo chmod +x install-minimal.sh
+sudo chmod +x install-programs.sh
 ./install-programs.sh
 sudo chmod +x config.sh
-./config-std.sh
+./config.sh
 sudo chmod +x config-extra.sh
 ./config-extra.sh
 sudo chmod +x gen-wifi-std.sh
