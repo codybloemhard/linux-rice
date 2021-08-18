efa63794d28b6b6c01fd0e01533da264be98a23a
Cody Bloemhard <cblmhrd@live.nl>
2021-08-18 22:18:40 +0200

    changes in scripts, new theme: tokyo-night, selecting is a bit wack due full transparant bg

--- scripts/record.sh
+++ scripts/record.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-ffmpeg -f x11grab -video_size 2560x1440 -framerate 25 -i $DISPLAY -f pulse -i default -c:v libx264 -preset superfast -c:a aac screen.mp4
+ffmpeg -video_size 2560x1440 -framerate 30 -f x11grab -i :0.0+0,0 output.mp4
